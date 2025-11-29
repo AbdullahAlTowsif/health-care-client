c9536bf505f49c9cc8dff948e18c0fbe80398dd7
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-11-30 01:36:09 +0600

    consultation & doctor details added

---
+++ src/app/(commonLayout)/consultation/doctor/[id]/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DoctorDetailLoading = () => {
+    return (
+        <div>
+            DoctorDetailLoading
+        </div>
+    );
+};
+
+export default DoctorDetailLoading;
